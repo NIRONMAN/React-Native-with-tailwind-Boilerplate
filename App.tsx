da47f2225eacd26dc692fe352fdcd766d174eca4
ucrem <davide.mercuri@openconnexio.com>
2024-01-09 18:57:59 +0100

    Main ok

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, Text, TextInput, View} from 'react-native';
+
+import {Text, View} from 'react-native';
 
 import { NativeWindStyleSheet } from "nativewind";
-import {Image} from "expo-image";
-import {ButtonDefault} from "./components/ui/buttons/button";
+
 
 NativeWindStyleSheet.setOutput({
     default: "native",
@@ -11,44 +10,12 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <View className={"sm:mx-auto sm:w-full sm:max-w-md"}>
-            <Image
-                className={"mx-auto h-10 w-auto"}
-                source={"https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"}
-                contentFit={"cover"}
-                transition={1000}
-            />
-            <Text className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+    <View className={"w-full bg-white px-6 py-24 sm:py-32 lg:px-8"}>
+        <View className={"mx-auto max-w-2xl text-center"}>
+            <Text className={"text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"}>Expo Tailwind Boilerplate</Text>
+            <Text className={"mt-6 text-lg leading-8 text-gray-600"}>
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
             </Text>
-        </View>
-
-        <View className={"mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]"}>
-            <View className={"bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12"}>
-                <View className={"space-y-6"}>
-                    <View>
-                        <Text className={"block text-sm font-medium leading-6 text-gray-900"}>Email address</Text>
-                        <View className={"mt-2"}>
-                            <TextInput
-                                className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}
-                            />
-                        </View>
-                    </View>
-                    <View>
-                        <Text className={"block text-sm font-medium leading-6 text-gray-900"}>Password</Text>
-                        <View className={"mt-2"}>
-                            <TextInput
-                                secureTextEntry={true}
-                                className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}
-                            />
-                        </View>
-                    </View>
-                    <View>
-                        <ButtonDefault title={"Sign in"}/>
-                    </View>
-                </View>
-            </View>
         </View>
     </View>
   );
